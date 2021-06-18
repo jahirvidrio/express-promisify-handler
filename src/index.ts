689cf90f89e6a0b91ad978d13822910b40217ec8
Jahir Vidrio <jahir.vidrio@gmail.com>
2021-06-18 16:57:38 -0500

    feat: Ussing p-is-promisse to validate promise

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Handler, NextFunction, Request, Response } from 'express';
+import isPromise from 'p-is-promise';
 
+type PromiseHandler<T> = (req: Request<any, any, any, any, any>, res: Response<any, any>, next: NextFunction) => Promise<T>;
 
-export default (handler: <S = Request, T = Response, U = NextFunction, V = unknown>(req: S, res: T, next: U) => Promise<V>): Handler => (req, res, next) => {
-  handler(req, res, next)
-    .catch(next);
+export const promisify = <T = unknown>(handler: PromiseHandler<T>): Handler => (req, res, next) => {
+  if (isPromise(handler)) return handler(req, res, next).catch(next);
+
+  handler(req, res, next);
 };
