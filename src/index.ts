7d850c40f3e45808c00adec387cbbb1b1eac3546
Jahir Vidrio <jahir.vidrio@gmail.com>
2021-06-21 13:32:04 -0500

    dispense with p-is-promise

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Handler, NextFunction, Request, Response } from 'express';
-import isPromise from 'p-is-promise';
+
 
 type PromiseHandler<T> = (req: Request<any, any, any, any, any>, res: Response<any, any>, next: NextFunction) => Promise<T>;
 
-export default <T = unknown>(handler: PromiseHandler<T>): Handler => (req, res, next) => {
-  if (isPromise(handler)) return handler(req, res, next).catch(next);
-
-  handler(req, res, next);
+export default <T = unknown>(handler: PromiseHandler<T>): Handler => async (req, res, next) => {
+  try { await handler(req, res, next); }
+  catch (error) { next(error); }
 };
